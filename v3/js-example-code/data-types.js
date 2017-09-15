0c3bca5c2db3271e13269f6e4b3ce2ed09acb516
tim <manzoid@gmail.com>
2017-09-14 17:10:42 -0700

    clean up data types example

--- v3/js-example-code/data-types.js
+++ v3/js-example-code/data-types.js
@@ -12,11 +12,24 @@ var boolFalse = false;
 var nullVal = null;
 var undefVal = undefined;
 
-var lst = ['a', 'b', 3, 4, 5, 'f'];
+var list = ['a', 'b', 3, 4, 5, 'f'];
 
-var obj = {name: 'John', age: 35, hasChildren: true};
+var person = {
+  name: 'John',
+  age: 35,
+  hasChildren: true
+};
 
 var i = 5;
-var obj_lst = [i, {foo: i+1, poop: [1, 2, 3]}, {bar: i+2}];
+var obj_list = [
+  i,
+  {
+    foo: i + 1,
+    stuff: [1, 2, 3]
+  },
+  {
+    bar: i + 2
+  }
+];
 
-obj.name = 'Jane';
+person.name = 'Jane';
